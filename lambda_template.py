a9a6e7288c31602e244e8cb29e49bc1816b4c540
locast <locast@gmx.fr>
2019-05-20 19:37:56 +0200

    lambda correcly getting ip and tags, correctly writing inventory, ### TO DO ### - read inventory

--- lambda_template.py
+++ lambda_template.py
@@ -31,7 +31,7 @@ def lambda_handler(event, context):
         region = event['region']
         instance_id = event['detail']['instance-id']
     except Exception as error:    
-        logger.error('principalId: ' + str(error))
+        logger.error(error)
     
     ec2 = boto3.resource('ec2')
 
@@ -43,8 +43,7 @@ def lambda_handler(event, context):
         #instance_sshkey_name = 
 
     except Exception as error:
-        logger.error('error: ' + str(error))
-
+        logger.error(error)
         #default value to try the rest waiting authorisations
         if debug:
             instance_tags = {'debug':'true', 'project':'docker-mlm','role':'manager'}
@@ -53,12 +52,12 @@ def lambda_handler(event, context):
 
     if debug:
         logger.info('tags: ' + str(instance_tags))
-        logger.info('PIp: ' + str(instance_private_ip))
+        logger.info('PIp: ' + str(instance_ip))
 
     # check if the created instance is part of project
     if target_tag_key in instance_tags:
         if target_tag_value == instance_tags[target_tag_key]:
-            update_inventory()
+            update_inventory(instance_tags, instance_ip, instance_user, instance_sshkey)
 
 #Read and Update the inventory file(aws S3)
 def update_inventory(instance_tags, instance_ip, instance_user, instance_sshkey):
@@ -67,8 +66,11 @@ def update_inventory(instance_tags, instance_ip, instance_user, instance_sshkey)
     ansible_grp = '['+role+']\n'
 
     ### TODO ####
-    READ INVENTORY IN S3
+    #READ INVENTORY IN S3#
     ##############
+
+    inventory = []
+
 
     inventory_add_line = '{0} ansible_ssh_user={1} ansible_ssh_key={2}\n'.format(instance_ip, instance_user, instance_sshkey)
 
@@ -77,56 +79,66 @@ def update_inventory(instance_tags, instance_ip, instance_user, instance_sshkey)
         logger.info('ansible_grp: ' + str(ansible_grp))
         logger.info('inventory_line: ' + str(inventory_add_line))
 ############ NEED TO READ FILE IN S3 ##################################
-#        if debug:
-#            logger.info('Ansible group found in inventory')
-#
-#        index = inventory.index(ansible_grp)
-#        index = insert = index + 1
-#        present = False
-#        file_line = inventory[index]
-#
-#        if debug:
-#            logger.info('ansible grp found in position: ' + str(index))
-#
-#        while file_line[0] != '[' and index < len(inventory):
-#            file_line = inventory[index]
-#
-#            if debug:
-#                logger.info('line in check for doublon: ' + str(file_line))
-#            # check if the ip adress already is in the inventory
-#            if file_line.split(' ')[0] == instance_ip:
-#                present = True
-#                index += 1
-#
-#                if debug:
-#                    logger.info('present: ' + str(present))
-#                    logger.info('instance_ip: ' + str(instance_ip))
-#                    logger.info('file_line: ' + file_line.split(' '))
-#        # add line to inventory if ip not found
-#        if not present:
-#            inventory.insert(insert, line)
-############################################################################
-            # Add the new group and device t the ansible inventory
-            else:
-                if debug:
-                    logger.info('Ansible group not found in inventory')
+    if ansible_grp in inventory:
+        pass
+        """
+        if debug:
+            logger.info('Ansible group found in inventory')
 
-                inventory.append(ansible_grp)
-                inventory.append(line)
+        index = inventory.index(ansible_grp)
+        index = insert = index + 1
+        present = False
+        file_line = inventory[index]
+
+        if debug:
+            logger.info('ansible grp found in position: ' + str(index))
+
+        while file_line[0] != '[' and index < len(inventory):
+            file_line = inventory[index]
+
+            if debug:
+                logger.info('line in check for doublon: ' + str(file_line))
+            # check if the ip adress already is in the inventory
+            if file_line.split(' ')[0] == instance_ip:
+                present = True
+                index += 1
+
+                if debug:
+                    logger.info('present: ' + str(present))
+                    logger.info('instance_ip: ' + str(instance_ip))
+                    logger.info('file_line: ' + file_line.split(' '))
+        # add line to inventory if ip not found
+        if not present:
+            inventory.insert(insert, inventory_add_line)
+        """
+############################################################################
+    # Add the new group and device t the ansible inventory
+    else:
+        if debug:
+            logger.info('Ansible group not found in inventory')
+
+        inventory.append(ansible_grp)
+        inventory.append(inventory_add_line)
 
 ##### DirecT IN S3 1 GO ###################################
-            # Write the new inventory in /tmp
-            with open('/tmp/inventory.inv','w') as inventory_file:
-                inventory = inventory_file.writelines(inventory)
+        # Write the new inventory in /tmp
+        with open('/tmp/inventory.inv','w') as inventory_file:
+            inventory_file.writelines(inventory)
+            """
+            if debug:
+                logger.info('final inventory: ' + str(inventory))
+            """
+        # Create an S3 client
+        s3 = boto3.client('s3')
 
-            # Create an S3 client
-            s3 = boto3.client('s3')
-
-            #Upload inventory to S3
+        #Upload inventory to S3
+        try:
             s3.upload_file('/tmp/inventory.inv', bucket_name, 'inventory.inv')
-
+        except Exception as error:
+            logger.error(error)
+        else:
+            logger.info('invotory writen in S3')
 ##############################################
-
 """
 client = boto3.client('s3') #low-level functional API
 resource = boto3.resource('s3') #high-level object-oriented API
